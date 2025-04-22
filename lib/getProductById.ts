38d2fe0e1c1ea1f14a1185f0b690f737c88854e3
RogelioRamir <rogelio.ramirez@inchcape.com>
2025-04-22 00:10:33 -0500

    Backup antes de codemod

--- lib/getProductById.ts
+++ lib/getProductById.ts
@@ -2,6 +2,6 @@ import { Product } from '@/types/product'
 
 export async function getProductById(id: string): Promise<Product | undefined> {
     const productsData = await import('@/data/mock-products.json')
-    const products = productsData.default as Product[]
-    return products.find((p) => p.id === id)
+    return (productsData.default as Product[]).find((p) => p.id === id)
 }
+
